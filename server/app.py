4897ff0276f657d5450775ee1ed022187fa0fdab
Mason Barnes <barnesmason980@gmail.com>
2024-10-02 17:36:56 -0700

    init

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Use SQLite for simplicity
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+CORS(app)
+
+
+migrate = Migrate(app, db)
