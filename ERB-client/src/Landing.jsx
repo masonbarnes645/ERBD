c46698a2deca39dbea86f4af181065b94d4d008f
Mason Barnes <barnesmason980@gmail.com>
2024-10-03 16:02:56 -0700

    frontend router set up

---
+++ ERB-client/src/Landing.jsx
@@ -0,0 +1,15 @@
+
+
+const Landing = () =>
+{
+
+
+    return(
+
+        <div>
+            <h4>suck my dick</h4>
+        </div>
+    )
+}
+
+export default Landing
--- ERB-client/src/main.jsx
+++ ERB-client/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { router } from "./routes";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<RouterProvider router={router} />);
