210080ab67093e2f57eaa2bca4eb103da10f40b9
Mason Barnes <barnesmason980@gmail.com>
2024-10-04 16:50:17 -0700

    toast + portfolio FE fetch

--- ERB-client/src/App.jsx
+++ ERB-client/src/App.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { Toaster } from "react-hot-toast"
 
 const App = () => {
 
   return(
+   <>
     <div>
       <Outlet />
     </div>
+    <Toaster />
+    </> 
   )
 
 }
--- ERB-client/src/Portfolio.jsx
+++ ERB-client/src/Portfolio.jsx
@@ -1,7 +1,23 @@
-
+import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
 
 const Portfolio = () =>{
+    const [portfolios, setPortfolios] = useState()
 
+    useEffect(() => {
+        fetch("http://127.0.0.1:5555/portfolios")
+            .then((res) => {
+                if (res.ok) {
+                    res.json().then(setPortfolios)
+                }
+                else {
+                    res.json().then((errObj) => toast.error(errObj.error))
+                }
+            })
+            .catch((errObj) => toast.error(errObj.error))
+    }, [])
+
+    console.log(portfolios)
     return(
         <div>
             <h4>blow me</h4>
