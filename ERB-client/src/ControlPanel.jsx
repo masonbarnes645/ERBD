afe6c703bfabab38865edef565430acb833286d0
Mason Barnes <barnesmason980@gmail.com>
2024-10-07 16:49:47 -0700

    product form submit v1 works

--- ERB-client/src/ControlPanel.jsx
+++ ERB-client/src/ControlPanel.jsx
@@ -1,6 +1,7 @@
 // list products, add to products, delete products, analytics?
 
 import { useState } from "react"
+import { postProduct } from "./api";
 
 // for scrolling container, css = overflow-y
 // conditional post for portfolio or product
@@ -8,49 +9,56 @@ import { useState } from "react"
 
 const ControlPanel = () => {
     const [postFormData, setPostFormData] = useState({
-        "title": "",
-        "description": ""
+
     });
     const [productFormOpen, setProductFormOpen] = useState(false)
     const [portfolioFormOpen, setPortfolioFormOpen] = useState(false)
 
 
-    const handleChange = (e) =>{
-        const {name, value} = e.target
+    const handleChange = (e) => {
+        const { name, value } = e.target
         setPostFormData((prevData) => ({
             ...prevData,
             [name]: value
         })
-    )
+        )
     }
 
-    const toggleForm = (e) =>{
+    const toggleForm = (e) => {
         console.log(e.target.name)
-        if(e.target.name == "product")
+        if (e.target.name == "product")
             setProductFormOpen((current) => (!current))
         else if (e.target.name == "portfolio")
             setPortfolioFormOpen((current) => (!current))
+    }
+
+    const handleSubmit = async (postFormData, e) =>{
+        e.preventDefault()    
+        await postProduct(postFormData)
 
 
     }
-
-
     
     
     
-    return(
-        <div>
-            <h4>control</h4>
-            <button onClick={toggleForm} name="product">dfgfdgd</button>
-            <button onClick={toggleForm} name="portfolio">dfgsssfdgd</button>
-            <form onSubmit={(e) => handleSubmit(e)}>
+    const productForm = (productFormOpen ? <form onSubmit={(e) => handleSubmit(postFormData, e)}>
         <label>
-            title: <input type="text" name="title" value={postFormData.title} onChange={handleChange} />
+            title: <input type="text" name="name" value={postFormData.title} onChange={handleChange} />
             description: <input type="text" name="description" value={postFormData.description} onChange={handleChange} />
+            price: <input type="integer" name="price" value={postFormData.price} onChange={handleChange} />
         </label>
         <button type="submit">Submit</button>
+    </form> : 
+    <></>)
 
-    </form>
+
+
+    return (
+        <div>
+            <h4>{productForm}</h4>
+            <button onClick={toggleForm} name="product">dfgfdgd</button>
+            <button onClick={toggleForm} name="portfolio">dfgsssfdgd</button>
+
         </div>
     )
 }
