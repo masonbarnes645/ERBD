303b3ecf534ce3f0761eb9e092d96c8201d33ba8
Mason Barnes <barnesmason980@gmail.com>
2024-10-21 15:45:46 -0600

    fix grid 2 and add contact skeleton

--- ERB-client/src/Contact.jsx
+++ ERB-client/src/Contact.jsx
@@ -1,12 +1,29 @@
+import { Box, Typography } from "@mui/material"
+import Grid from '@mui/material/Grid2';
 
 const Contact = () => {
 
 
-    
+
     return (
-        <div>
-            <h1>sdss</h1>
-        </div>
+        <Box alignItems={"center"}>
+            <Grid container spacing={2} alignItems={"center"} marginX={20} marginY={30} >
+                <Grid size={4}>
+                    <Box>
+                        <img src="./src/assets/headshotEBD.jpeg" />
+                        <Box maxWidth={'100%'}>
+                            <Typography variant="body1">
+                                About ElizabethBarnesDesign Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid size={8}>
+                    <h1>sfdsfd</h1>
+                </Grid>
+            </Grid>
+        </Box>
+
     )
 }
 
