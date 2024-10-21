6226f145ada258f07e64021cd17c903b6a418ca1
Mason Barnes <barnesmason980@gmail.com>
2024-10-20 22:18:56 -0600

    background image

--- ERB-client/src/Landing.jsx
+++ ERB-client/src/Landing.jsx
@@ -1,22 +1,42 @@
+import { Box, Container } from "@mui/material"
 import { Link } from "react-router-dom"
+import Camino1 from './assets/Camino_1.jpg';
 
-const Landing = () =>
-{
+const Landing = () => {
 
 
-    return(
+    return (
+        <Container sx={{
+            height: '100vh', 
+            width: '100vw', 
+            padding: 0,     
+            margin: 0,      
+        }}
+        >
+            <Box
+                sx={{
+                    backgroundImage: `url(${Camino1})`, 
+                    backgroundSize: 'cover',      
+                    backgroundPosition: 'center', 
+                    backgroundRepeat: 'no-repeat', 
+                    height: '100vh', 
+                    width: '100vw',
+                }}
+            >
 
-        <div>
-            <Link to={"/portfolios"}>
-                <button>Portfolio</button>
-            </Link>
-            <Link to={"/products"}>
-                <button>View Products</button>
-            </Link>
-            <Link to={"/about-us"}>
-                <button>About EBD</button>
-            </Link>
-        </div>
+            </Box>
+            <Box>
+                <Link to={"/portfolios"}>
+                    <button>Portfolio</button>
+                </Link>
+                <Link to={"/products"}>
+                    <button>View Products</button>
+                </Link>
+                <Link to={"/about-us"}>
+                    <button>About EBD</button>
+                </Link>
+            </Box>
+        </Container>
     )
 }
 
--- ERB-client/src/NavBar.jsx
+++ ERB-client/src/NavBar.jsx
@@ -1,21 +1,47 @@
 import { useNavigate } from "react-router-dom";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import AppBar from "@mui/material/AppBar";
+import './App.css';
+import { Box, Container, createTheme, ThemeProvider } from "@mui/material";
 
 
+const theme = createTheme({
+    components:{
+        MuiButton:{
+            styleOverrides:{
+                root:{
+                    fontSize: "18px",
+                    color: "aliceblue",
+                    marginRight:90,
+                    width:150
+                    
+                }
+            }
+        }
+    }   
+}
+)
 
 const NavBar = () => {
     const navigate = useNavigate()
-    return(
-        <div className="navigation-bar">
+    return (
+        <Container fixed>
+        <AppBar>
+            <Box display="flex" justifyContent="space-between" sx={{paddingY:2}}>
             <img src="src/assets/Header_logo.png" alt="header" />
-            <Stack spacing={2} direction="row">
-                <Button variant="text" onClick={() => navigate("/")}> Home </Button>
-                <button onClick={() => navigate("/products")}> Products </button>
-                <button onClick={() => navigate("/portfolios")}> Portfolio </button>
-            </Stack>
-        </div>
+                <ThemeProvider theme={theme}>
+                <Box sx={{paddingTop:2}}>
+                    <Button variant="text" onClick={() => navigate("/")} className="navbutton"> Home </Button>
+                    <Button variant="text" onClick={() => navigate("/products")} className="navbutton"> Products </Button>
+                    <Button variant="text" onClick={() => navigate("/portfolios")} className="navbutton"> Portfolio </Button>
+                </Box>
+                </ThemeProvider>
+            </Box>
+        </AppBar>
+        </Container>
     )
 }
+
 
 export default NavBar   
