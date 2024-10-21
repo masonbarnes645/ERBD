30dc0552d3bc380c919dc8c8ef537b91b434ef92
Mason Barnes <barnesmason980@gmail.com>
2024-10-20 22:29:16 -0600

    pivot

--- ERB-client/src/Landing.jsx
+++ ERB-client/src/Landing.jsx
@@ -1,42 +1,22 @@
-import { Box, Container } from "@mui/material"
 import { Link } from "react-router-dom"
-import Camino1 from './assets/Camino_1.jpg';
 
-const Landing = () => {
+const Landing = () =>
+{
 
 
-    return (
-        <Container sx={{
-            height: '100vh', 
-            width: '100vw', 
-            padding: 0,     
-            margin: 0,      
-        }}
-        >
-            <Box
-                sx={{
-                    backgroundImage: `url(${Camino1})`, 
-                    backgroundSize: 'cover',      
-                    backgroundPosition: 'center', 
-                    backgroundRepeat: 'no-repeat', 
-                    height: '100vh', 
-                    width: '100vw',
-                }}
-            >
+    return(
 
-            </Box>
-            <Box>
-                <Link to={"/portfolios"}>
-                    <button>Portfolio</button>
-                </Link>
-                <Link to={"/products"}>
-                    <button>View Products</button>
-                </Link>
-                <Link to={"/about-us"}>
-                    <button>About EBD</button>
-                </Link>
-            </Box>
-        </Container>
+        <div>
+            <Link to={"/portfolios"}>
+                <button>Portfolio</button>
+            </Link>
+            <Link to={"/products"}>
+                <button>View Products</button>
+            </Link>
+            <Link to={"/about-us"}>
+                <button>About EBD</button>
+            </Link>
+        </div>
     )
 }
 
