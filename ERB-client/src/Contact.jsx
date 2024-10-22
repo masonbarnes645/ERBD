d1d8e62b2df1333bdd3ea52c424e336ce0a6a961
Mason Barnes <barnesmason980@gmail.com>
2024-10-21 21:57:03 -0600

    form skeleton

--- ERB-client/src/Contact.jsx
+++ ERB-client/src/Contact.jsx
@@ -1,5 +1,9 @@
-import { Box, Typography } from "@mui/material"
 import Grid from '@mui/material/Grid2';
+import Box from '@mui/material/Box';
+import Typography from "@mui/material/Typography";
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import { width } from '@mui/system';
 
 const Contact = () => {
 
@@ -7,11 +11,11 @@ const Contact = () => {
 
     return (
         <Box alignItems={"center"}>
-            <Grid container spacing={2} alignItems={"center"} marginX={20} marginY={30} >
+            <Grid container spacing={2} alignItems={"center"} marginX={20} marginY={20} >
                 <Grid size={4}>
                     <Box>
                         <img src="./src/assets/headshotEBD.jpeg" />
-                        <Box maxWidth={'100%'}>
+                        <Box maxWidth={'80%'}>
                             <Typography variant="body1">
                                 About ElizabethBarnesDesign Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
                             </Typography>
@@ -19,7 +23,21 @@ const Contact = () => {
                     </Box>
                 </Grid>
                 <Grid size={8}>
-                    <h1>sfdsfd</h1>
+                    <Paper component='form' sx={{ width: '100%', height: 400, paddingTop:8 }} >
+                        <Box alignContent={'center'}>
+                            <TextField label="First Name" />
+                            <TextField label="Last Name" />
+                        </Box>
+                        <Box>
+                            <TextField label="Email"/>
+                        </Box>
+                        <Box>
+                            <TextField label="Subject"/>
+                        </Box>
+                        <Box>
+                            <TextField label="Message" sx={{width:'80%', marginX:"5%"}}/>
+                        </Box>
+                    </Paper>
                 </Grid>
             </Grid>
         </Box>
--- ERB-client/src/Landing.jsx
+++ ERB-client/src/Landing.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom"
-import Container from '@mui/material/Container'
 import Box from "@mui/material/Box"
 import { Button, Grid2, Typography } from "@mui/material"
 
