b287020b34ce4634e37c19c7e3f61cf7cc4914e2
Mason Barnes <barnesmason980@gmail.com>
2024-11-07 10:21:21 -0800

    footer

--- ERB-client/src/Footer.jsx
+++ ERB-client/src/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
 
     return (
 
-        <Box sx={{color:'black', display:'flex', justifyContent:'flex-end', borderTop:'2px solid grey', marginTop:'2rem' }}>
-            <Box marginRight={'2rem'}>
-                <p>blow me</p>
+        <Box sx={{color:'black', display:'flex', justifyContent:'flex-end',  marginTop:'2rem' }}>
+            <Box marginRight={'2rem'} fontSize={'.8rem'} >
+                <p>Interior Design |  Elizabeth Barnes Design ©2017 | email: betsy@elizabethbarnesdesign.com  |  phone: 510.301.2740</p>
             </Box>
         </Box>
 
