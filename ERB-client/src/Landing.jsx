d8be65168ade0c6d0e4fd1e37720916b2c499657
Mason Barnes <barnesmason980@gmail.com>
2024-11-08 19:16:06 -0800

    carousel dependencies set up

--- ERB-client/src/Landing.jsx
+++ ERB-client/src/Landing.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import Box from "@mui/material/Box"
 import { Button, Container, Typography } from "@mui/material"
-import Grid from '@mui/material/Grid2';
+import Carousel from "./Carousel"
 import './App.css'
 import { blue } from "@mui/material/colors";
 
@@ -10,35 +10,9 @@ const Landing = () => {
 
 
     return (
-        < Container className="landing-grid" maxWidth={false} sx={{  width: '100vw', height: '100vh' }}>
-            <Box className="zen-font" sx={{
-                gridColumnStart: 4, gridColumnEnd: 14, textAlign: 'center' }}>
-                <h1>Elizabeth Barnes Design</h1>
-            </Box>
-            <Box sx={{ gridRowStart: 2, gridRowEnd: 10, gridColumnStart: 5, gridColumnEnd: 13, borderStyle:'solid' }} >
-                <img src="http://localhost:5555/uploads/Camino_1.jpg" id="landing-pic" />
-            </Box>
-            <Box className="zen-font" display={"flex"} sx={{
-                gridRowStart: 10,
-                gridColumnStart: 5,
-                gridColumnEnd: 13,
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                flexFlow: { xs: "column", sm: 'row' }
-            }}>
-                <Link to={"/portfolios"}>
-                    <Button  variant="contained" size="large" sx={{ backgroundColor: '#36454F', marginY: { xs: '10px', sm: '0px' } }}> View our Work</Button>
-                </Link>
-                <Link to={"/products"}>
-                    <Button variant="contained" size="large"sx={{ backgroundColor: '#36454F', marginBottom: { xs: '10px', sm: '0px' } }}> View Furniture</Button>
-                </Link>
-                <Link to={"/contact-us"}>
-                    <Button variant="contained" size="large"sx={{ backgroundColor: '#36454F' }}>
-                        Contact Us
-                    </Button>
-                </Link>
-            </Box>
-        </Container>
+        <Box>
+            <Carousel />
+        </Box>
     )
 }
 
