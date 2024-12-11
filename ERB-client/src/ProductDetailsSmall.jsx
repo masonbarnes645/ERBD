b4c3b55bc4b6c86a58840c1fec150638aefe087e
Mason Barnes <barnesmason980@gmail.com>
2024-12-11 11:05:59 -0800

    change src for product images

--- ERB-client/src/ProductDetailsSmall.jsx
+++ ERB-client/src/ProductDetailsSmall.jsx
@@ -14,12 +14,12 @@ const ProductDetailsSmall = ({ product }) => {
             <Box display={'flex'} flexDirection={'column'} alignItems={'center'} width={'100%'} overflow={'hidden'}>
                 <Box sx={{ objectFit: 'cover', justifyContent: 'center' }}>
                     {product.photos && product.photos[mainPhotoId] && (
-                        <img style={{maxWidth:'100%'}} src={`http://localhost:5555/${product.photos[mainPhotoId].file_path}`} />
+                        <img style={{maxWidth:'100%'}} src={`api/v1/${product.photos[mainPhotoId].file_path}`} />
                     )}
                 </Box>
 
                 <Box display={'flex'} flexDirection={'row'} justifyContent={'space-around'} marginTop={'1rem'}>
-                    {product.photos?.slice(0, 2).map((photo, index) => (<Box key={photo.id} sx={{ borderStyle: 'solid' }}> <img style={{ height: 'auto', width: '60px' }} src={`http://localhost:5555/${photo.file_path}`} onClick={() => handleClick(index)} /> </Box>))}
+                    {product.photos?.slice(0, 2).map((photo, index) => (<Box key={photo.id} sx={{ borderStyle: 'solid' }}> <img style={{ height: 'auto', width: '60px' }} src={`api/v1/${photo.file_path}`} onClick={() => handleClick(index)} /> </Box>))}
                 </Box>
                 <Box >
                     ${product.price}
