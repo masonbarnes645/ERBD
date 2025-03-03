7ad7028513b4b18548cdf64c404ce1671d856b35
Mason Barnes <barnesmason980@gmail.com>
2025-03-03 15:13:15 -0800

    useeffect to fetch portfolio for edit

--- ERB-client/src/EditPortfolio.jsx
+++ ERB-client/src/EditPortfolio.jsx
@@ -1,6 +1,20 @@
 
 
 const EditPortfolio = () => {
+    const { portfolioId } = useParams()
+    const [portfolio, setPortfolio] = useState({
+        photos: []
+    })
+
+    useEffect(() => {
+        const loadPortfolio = async () => {
+            if (!portfolioId) return;
+            const data = await fetchPortfolioById(portfolioId);
+            setPortfolio(data);
+        };
+
+        loadPortfolio();
+    }, [])
 
     return     (
         <>
