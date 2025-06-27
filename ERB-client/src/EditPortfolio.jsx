69671d4e0d1a718cb04afc753dd1b137d6754080
Mason Barnes <barnesmason980@gmail.com>
2025-06-27 09:45:26 -0700

    portfolio detail page

--- ERB-client/src/EditPortfolio.jsx
+++ ERB-client/src/EditPortfolio.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchPortfolioById } from "./api";
+import { Box, ImageList, ImageListItem, Typography } from "@mui/material";
 
 
 const EditPortfolio = () => {
@@ -19,10 +20,21 @@ const EditPortfolio = () => {
         loadPortfolio();
     }, [])
 
-    return     (
-        <>
-        <p>{portfolio.title}</p>
-        </>
+    return (
+        <Box marginTop={'5rem'} >
+            <Box textAlign={'center'} color={'gray'} borderBottom={'solid'}>
+                <Typography sx={{ fontSize: { lg: '5rem', md: '4rem', sm: '3rem', xs: '1.5rem' }, fontFamily: 'Zen Antique' }}>
+                    {portfolio.title}
+                </Typography>
+            </Box>
+            <ImageList variant="standard" gap={6} sx={{ marginX: '1rem', marginTop: '2rem' }}>
+                {portfolio.photos.map((photo) => (
+                    <ImageListItem key={photo.id}>
+                        <img src={photo.file_url} alt={photo.title} />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+        </Box>
     )
 }
 
