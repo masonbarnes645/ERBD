3db94f3d56f15848059e29e6e4c26905c2268867
Mason Barnes <barnesmason980@gmail.com>
2025-12-03 12:36:58 -0800

    email typo fix

--- ERB-client/src/Footer.jsx
+++ ERB-client/src/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
         location.pathname === '/'? null :
         <Box sx={{color:'black', display:'flex', justifyContent:'flex-start',  paddingTop:'2rem', width:'100%', bottom:0}}>
             <Box fontSize={'.8rem'}  width={'100%'} marginX={'1rem'}>
-                <p>Interior Design |  Elizabeth Barnes Design ©2017 | email: betsy@elizabethbarnesdesign.com  |  phone: 510.301.2740</p>
+                <p>Interior Design |  Elizabeth Barnes Design ©2017 | email: betsy@ebarnesdesign.com  |  phone: 510.301.2740</p>
             </Box>
         </Box>
 
